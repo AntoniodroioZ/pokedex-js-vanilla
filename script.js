15bbd1eded905874200e472bfff7e7d8ea4874f3
AntoniodroioZ <antonybey13@gmail.com>
2022-07-26 22:30:14 -0500

    Service worker

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// // service worker
-// if('serviceWorker'in navigator){
-//     navigator.serviceWorker.register('./sw.js')
-//     .then(reg=>console.log('Registro de Service Worker existoso', reg))
-//     .then(err=>console.warn('Error al registrar el SW',err));
-// }
+// service worker
+if('serviceWorker'in navigator){
+    navigator.serviceWorker.register('./sw.js')
+    .then(reg=>console.log('Registro de Service Worker existoso', reg))
+    .then(err=>console.warn('Error al registrar el SW',err));
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('input[type=text]').forEach( node => node.addEventListener('keypress', e => {
